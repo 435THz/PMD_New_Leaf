e87c08c99ac7a5a555b77e2a0e0ad9e0b27f24c4
MistressNebula <delbovemarie@gmail.com>
2023-12-28 16:57:42 +0100

    Prepared emergency hub warp

--- Data/Script/Global.lua
+++ Data/Script/Global.lua
@@ -8,6 +8,7 @@ GLOBAL = {
     HubData = {
         LevelRankTable = {1,1,2,2,2,3,3,3,4,4},
         RankName = {"Camp", "Village", "Town", "City"},
+        RankHubMap = {"hub_small", "hub_medium", "hub_large", "hub_final"},
         LevelBuildLimit = {2,3,4,5,6,7,8,10,12,15},
         LevelAssemblyLimit = {10,25,40,60,80,100,150,200,300,500},
         BoardUnlockLevel = 3
@@ -25,6 +26,10 @@ end
 
 function GLOBAL.getHubSuffix()
     return GLOBAL.HubData.RankName[GLOBAL.getHubRank()]
+end
+
+function GLOBAL.getHubMap()
+    return GLOBAL.HubData.RankHubMap[GLOBAL.getHubRank()]
 end
 
 function GLOBAL.getHubName()
