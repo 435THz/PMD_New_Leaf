9e00808c0fd983a15794e041968be507c676eabf
MistressNebula <delbovemarie@gmail.com>
2024-02-18 13:35:10 +0100

    Moved HubData to HubManager.lua

--- Data/Script/Global.lua
+++ Data/Script/Global.lua
@@ -5,33 +5,5 @@ require 'CommonFunctions'
 ]]
 GLOBAL = {
     GenderTable = {RogueEssence.Data.Gender.Male, RogueEssence.Data.Gender.Female, RogueEssence.Data.Gender.Genderless},
-    HubData = {
-        LevelRankTable = {1,1,2,2,2,3,3,3,4,4},
-        RankName = {"Camp", "Village", "Town", "City"},
-        RankHubMap = {"hub_small", "hub_medium", "hub_large", "hub_final"},
-        LevelBuildLimit = {2,3,4,5,6,7,8,10,12,15},
-        LevelAssemblyLimit = {10,25,40,60,80,100,150,200,300,500},
-        BoardUnlockLevel = 3
-    }
 }
 
-
--------------------------------------------
---region Save Data Reading
--------------------------------------------
-
-function GLOBAL.getHubRank()
-    return GLOBAL.HubData.LevelRankTable[SV.HubData.Level]
-end
-
-function GLOBAL.getHubSuffix()
-    return GLOBAL.HubData.RankName[GLOBAL.getHubRank()]
-end
-
-function GLOBAL.getHubMap()
-    return GLOBAL.HubData.RankHubMap[GLOBAL.getHubRank()]
-end
-
-function GLOBAL.getHubName()
-    return "[color=#FFFFA5]"..SV.HubData.Name..COMMON_FUNC.tri(SV.HubData.UseSuffix, " "..GLOBAL.getHubSuffix(), "").."[color]"
-end
---
+++ Data/Script/HubManager.lua
@@ -0,0 +1,202 @@
+require 'ShopManager'
+--[[
+    HubManager.lua
+    Contains all constant global tables and variables necessary for the Hub to function, plus some functions that interface with them in a more intuitive way.
+]]
+
+_HUB = {}
+
+_HUB.LevelRankTable = {1,1,2,2,2,3,3,3,4,4}
+_HUB.RankSuffix = {"Camp", "Village", "Town", "City"}
+_HUB.RankHubMap = {"hub_small", "hub_medium", "hub_large", "hub_final"}
+_HUB.LevelBuildLimit = {2,3,4,5,6,7,8,10,12,15}
+_HUB.LevelAssemblyLimit = {10,25,40,60,80,100,150,200,300,500}
+_HUB.BoardUnlockLevel = 3
+_HUB.PlotPositions = {
+    -- rank 1
+    {
+        {X=128, Y=64},  -- first plot is always "home"
+        {X=304, Y=64},  -- second plot is always "office"
+        {X=64,  Y=216}, -- plots 3+ have their plot index-2 as their id; this is the 3rd plot, but its id is "1"
+        {X=368, Y=216}, -- this is the 4th plot, but its id is "2"
+        {X=216, Y=216}  -- this is the 5th plot, but its id is "3"
+    },
+    -- rank 2, WIP
+    {
+        {X=128, Y=64},
+        {X=304, Y=64},
+        {X=64,  Y=216},
+        {X=368, Y=216},
+        {X=216, Y=216},
+        ---------------
+        {X=128, Y=64},
+        {X=304, Y=64},
+        {X=64,  Y=216}
+    },
+    -- rank 3, WIP
+    {
+        {X=128, Y=64},
+        {X=304, Y=64},
+        {X=64,  Y=216},
+        {X=368, Y=216},
+        {X=216, Y=216},
+        ---------------
+        {X=128, Y=64},
+        {X=304, Y=64},
+        {X=64,  Y=216},
+        {X=368, Y=216},
+        {X=216, Y=216},
+        {X=128, Y=64},
+        {X=304, Y=64}
+    },
+    -- rank 4, WIP
+    {
+        {X=128, Y=64},
+        {X=304, Y=64},
+        {X=64,  Y=216},
+        {X=368, Y=216},
+        {X=216, Y=216},
+        ---------------
+        {X=128, Y=64},
+        {X=304, Y=64},
+        {X=64,  Y=216},
+        {X=368, Y=216},
+        {X=216, Y=216},
+        {X=128, Y=64},
+        {X=304, Y=64},
+        {X=64,  Y=216},
+        {X=368, Y=216},
+        {X=216, Y=216},
+        {X=128, Y=64},
+        {X=304, Y=64}
+    }
+
+}
+
+-------------------------------------------
+--region Getters
+-------------------------------------------
+
+function _HUB.getHubRank()
+    return _HUB.HubData.LevelRankTable[SV.HubData.Level]
+end
+
+function _HUB.getHubSuffix()
+    return _HUB.RankSuffix[_HUB.getHubRank()]
+end
+
+function _HUB.getHubMap()
+    return _HUB.RankHubMap[_HUB.getHubRank()]
+end
+
+function _HUB.getBuildLimit()
+    return _HUB.LevelBuildLimit[_HUB.getHubRank()]
+end
+
+function _HUB.getAssemblyLimit()
+    return _HUB.LevelAssemblyLimit[_HUB.getHubRank()]
+end
+
+function _HUB.boardUnlocked()
+    return _HUB.getHubRank() >= _HUB.BoardUnlockLevel
+end
+
+function _HUB.getHubName()
+    return "[color=#FFFFA5]"..SV.HubData.Name..COMMON_FUNC.tri(SV.HubData.UseSuffix, " ".._HUB.getHubSuffix(), "").."[color]"
+end
+
+function _HUB.getPlotOriginList()
+    return _HUB.PlotPositions[_HUB.getHubRank()]
+end
+
+function _HUB.getPlotOrigin(plot_id)
+    if plot_id == "home" then return _HUB.getPlotOriginList()[1] end
+    if plot_id == "office" then return _HUB.getPlotOriginList()[2] end
+    return _HUB.getPlotOriginList()[plot_id]
+end
+
+-------------------------------------------
+--region Graphics
+-------------------------------------------
+
+function _HUB.GenerateShopElements(plot_id, building_data, pos)
+    local rank = _HUB.getPlotRank(building_data)
+    local graphics_data = _HUB.ShopBase[building_data.type].Graphics[rank]
+
+    local elements = {
+        deco_bottom = nil,
+        deco_top = nil,
+        npc = nil,
+        objects = {}
+    }
+    local bottom = RogueEssence.Content.ObjAnimData(graphics_data.Base, 1)
+    local sheet = RogueEssence.Content.GraphicsManager.GetObject(graphics_data.Base) --TODO this might be the solution
+    local size_x, size_y = sheet.Width, sheet.Height
+    local offset_x, offset_y = 96-size_x, 96-size_y
+
+    elements.deco_bottom = RogueEssence.Ground.GroundAnim(bottom, RogueElements.Loc(pos.X+offset_x, pos.Y+offset_y))
+
+    if graphics_data.TopLayer then
+        offset_x, offset_y = 0, 0 -- TODO wait for audino's answer or look for the solution
+        local top = RogueEssence.Content.ObjAnimData(graphics_data.TopLayer, 1)
+        elements.deco_top = RogueEssence.Ground.GroundAnim(top, RogueElements.Loc(pos.X+offset_x, pos.Y+offset_y))
+    end
+
+    if graphics_data.NPC_Loc then
+        local name = "NPC_"..plot_id
+        local x, y = graphics_data.NPC_Loc.X + pos.X, graphics_data.NPC_Loc.Y + pos.Y
+        local temp_monster = RogueEssence.Dungeon.MonsterID(building_data.shopkeeper, 0, "normal", Gender.Genderless)
+        elements.npc = RogueEssence.Ground.GroundChar(temp_monster, RogueElements.Loc(x, y), Direction.Down, name, building_data.shopkeeper)
+    end
+    for _, box in pairs(graphics_data.Bounds) do
+        local anim = RogueEssence.Content.ObjAnimData()
+        local x, y = pos.X+box.X, pos.Y+box.Y
+        local w, h = box.W, box.Y
+        local trigger = box.Trigger
+        if not trigger then trigger = RogueEssence.Ground.GroundEntity.EEntityTriggerTypes.None end
+        local rect = RogueElements.Rect.FromPoints(RogueElements.Loc(x,y), RogueElements.Loc(x+w, y+h))
+        local passable = not box.Solid
+        if box.Solid == nil then passable = false end
+        local name = box.Name.."_"..plot_id
+        local obj = RogueEssence.Ground.GroundObject(anim, RogueElements.Dir8.Down, rect, RogueElements.Loc(), trigger, passable, name)
+        table.insert(elements.objects, obj)
+    end
+    return elements
+end
+
+-------------------------------------------
+--region SV Interface
+-------------------------------------------
+
+function _HUB.initializePlotData()
+    SV.HubData.Plots = SV.HubData.Plots or {}
+    for i = 1, 15, 1 do
+        if not SV.HubData.Plots[i] then
+            local plot_base = {
+                unlocked = i<_HUB.LevelBuildLimit[1],
+                building = "",
+                upgrades = {},
+                shopkeeper = "",
+                data = {}
+            }
+            table.insert(SV.HubData.Plots, plot_base)
+        end
+    end
+end
+
+function _HUB.getPlotData(index)
+    return SV.HubData.Plots[index]
+end
+
+function _HUB.getPlotLevel(plot)
+    local lvl = 0
+    for _, upgrade in plot.upgrades do
+        lvl = lvl + upgrade.count
+    end
+    return lvl
+end
+
+function _HUB.getPlotRank(plot)
+    local lvl = _HUB.getPlotLevel(plot)
+    return _HUB.LevelRankTable[lvl]
+end
