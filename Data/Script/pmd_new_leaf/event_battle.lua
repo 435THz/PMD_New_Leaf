2268e39321a7c91c8f4a761b4572b3ae4ae02a6a
MistressNebula <delbovemarie@gmail.com>
2024-12-06 15:58:14 +0100

    Fix require

--- Data/Script/pmd_new_leaf/event_battle.lua
+++ Data/Script/pmd_new_leaf/event_battle.lua
@@ -1,6 +1,6 @@
 require 'pmd_new_leaf.Global'
 require 'pmd_new_leaf.HubManager'
-require 'pmd_new_leaf.CommonFunctions.lua'
+require 'pmd_new_leaf.CommonFunctions'
 
 function BATTLE_SCRIPT.SaveCatchData(_, _, context, _)
     if context.Target.MemberTeam == _DATA.Save.ActiveTeam then
